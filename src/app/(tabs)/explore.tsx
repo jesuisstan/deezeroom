a2d013da0c1957cf505ac1be7f87a0ee0d257587
jesuisstan <stan.krivtsov@gmail.com>
2025-09-01 14:42:34 +0200

    DeezerPreviewPlayer-del-imports

--- src/app/(tabs)/explore.tsx
+++ src/app/(tabs)/explore.tsx
@@ -1,19 +1,11 @@
-import { Image, Platform } from 'react-native';
-
-import { Collapsible } from '@/components/ui/Collapsible';
-import { ExternalLink } from '@/components/ui/ExternalLink';
 import ParallaxScrollView from '@/components/ui/ParallaxScrollView';
 import { ThemedText } from '@/components/ui/ThemedText';
 import { ThemedView } from '@/components/ui/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import shootAlert from '@/utils/shoot-alert';
-import { useUser } from '@/contexts/UserContext';
 import DeezerPreviewPlayer from '@/components/DeezerPreviewPlayer';
 
-
 export default function TabTwoScreen() {
-  const { user, profile, profileLoading, updateProfile } = useUser();
-
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
